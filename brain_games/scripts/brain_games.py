6bfa0af898f8c0d23da56c95091ed8313a27a159
Denis Kulikov <neasan.08@mail.ru>
2023-08-15 06:40:08 +0500

    modified brain_games.py

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
-import brain_games.cli 
+import brain_games.cli
+
 
 def main():
     print('Welcome to the Brain Games!')
     print(f'Hello, {brain_games.cli.welcome_user()}!')
+
 
 if __name__ == 'main':
     main()
