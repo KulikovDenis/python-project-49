bf410e6b048cd04316edbc6fc7bf8fe903a34495
Denis Kulikov <neasan.08@mail.ru>
2023-08-19 11:54:29 +0500

    modified brain_games/brain_even_code.py

--- brain_games/brain_even_code.py
+++ brain_games/brain_even_code.py
@@ -2,11 +2,13 @@ import prompt
 
 print('Welcome to the Brain Games!')
 
+
 def gretting():
   global name
   name = prompt.string('May I have your name? ')
   print('Hello, ' + name + '!')
-  
+
+
 gretting()
 
 print('Answer "yes" if the number is even, otherwise answer "no".')
