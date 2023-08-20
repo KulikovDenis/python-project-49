d559089d916001518ba2334bd79c4e0e72ea94c5
Denis Kulikov <neasan.08@mail.ru>
2023-08-20 13:08:30 +0500

    add brain_games/scripts/brain_calc.py

---
+++ brain_games/scripts/brain_calc.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import brain_games.brain_code
+import brain_games.games.calc
+
+
+print('What is the result of the expression?')
+
+
+def main():
+    brain_games.games.calc.round()
+
+
+if __name__ == '__main__':
+    main()
