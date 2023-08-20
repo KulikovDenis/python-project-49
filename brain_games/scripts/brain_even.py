934836399c9120ab326c97a6d8c272460f95e051
Denis Kulikov <neasan.08@mail.ru>
2023-08-20 13:11:16 +0500

    modified brain_games/scripts/brain_even.py

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
-import brain_games.brain_even_code
-import brain_games.even
+import brain_games.brain_code
+import brain_games.games.even
+
+
+print('Answer "yes" if the number is even, otherwise answer "no".')
 
 
 def main():
-    brain_games.even.raund()
+    brain_games.games.even.round()
 
 
 if __name__ == '__main__':
