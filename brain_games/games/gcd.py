5bb16e6eaae8302af7c9cb292cb31f6156dcc915
Denis Kulikov <neasan.08@mail.ru>
2023-08-21 06:39:27 +0500

    add brain_games/games/gcd.py

---
+++ brain_games/games/gcd.py
@@ -0,0 +1,21 @@
+from random import randint
+import brain_games.brain_code
+from math import gcd
+
+
+def round():
+  i = 0
+  while i < 3:
+    random_number_1 = randint(1, 10)
+    random_number_2 = randint(1, 10)
+    print(f'Question: {random_number_1} {random_number_2}')
+    answer = input('Your answer: ')
+    if gcd(int(random_number_1), int(random_number_2)) == int(answer):
+      print('Correct!')  
+    else:
+      print(answer + ' is wrong answer ;(. Correct answer was ' + str(gcd(int(random_number_1), int(random_number_2))) + '.')
+      print("Let's try again, " + brain_games.brain_code.name + "!")
+      break
+    i = i + 1
+  if i == 3:
+    print('Congratulations, ' + brain_games.brain_code.name + '!')
