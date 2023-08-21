974756c6e693607445157a92c2872648d2715b07
Denis Kulikov <neasan.08@mail.ru>
2023-08-21 17:08:05 +0500

    add brain_games/scripts/brain_progression.py

---
+++ brain_games/scripts/brain_progression.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import brain_games.brain_code
+import brain_games.games.progression
+
+
+print('What number is missing in the progression?')
+
+
+def main():
+    brain_games.games.progression.round()
+
+
+if __name__ == '__main__':
+    main()
