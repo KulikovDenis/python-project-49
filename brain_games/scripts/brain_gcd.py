ea5d01c1bed5ed85191f7609e46f607d89b4b3d5
Denis Kulikov <neasan.08@mail.ru>
2023-08-21 06:39:45 +0500

    add brain_games/scripts/brain_gcd.py

---
+++ brain_games/scripts/brain_gcd.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import brain_games.brain_code
+import brain_games.games.gcd
+
+
+print('Find the greatest common divisor of given numbers.')
+
+
+def main():
+    brain_games.games.gcd.round()
+
+
+if __name__ == '__main__':
+    main()
