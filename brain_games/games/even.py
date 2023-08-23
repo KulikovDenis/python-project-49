06acb928f46def77cd9a9ff170b8c6a26b940ed4
Denis Kulikov <neasan.08@mail.ru>
2023-08-23 07:32:46 +0500

    modified file

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -20,11 +20,11 @@ def round():
                 else:
                     print("'yes' is wrong answer ;(. Correct answer was 'no'.")
                     print("Let's try again, " + brain_games.brain_code.name + "!")
-                    break
+                break
         else:
             print(answer + " is wrong answer ;(. Correct answer was 'yes' or no'.")
             print("Let's try again, " + brain_games.brain_code.name + "!")
-            break
+        break
         i = i + 1
     if i == 3:
         print('Congratulations, ' + brain_games.brain_code.name + '!')
